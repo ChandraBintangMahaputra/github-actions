ff1baabaac9003d7d4930dbf909f118ea5a8e3bd
Muhammad Chandra Bintang Mahaputra <mmahaputra@otsuka.id>
2025-05-05 22:06:10 +0700

    push

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, github-actions');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Chandra github-actions');
   });
 });
